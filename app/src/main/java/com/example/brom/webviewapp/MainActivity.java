3f80b8532358b37423bebbf5985a36da28aff4c0
a17eliri <a17eliri@student.his.se>
2018-03-26 10:51:52 +0200

    added an int and added standard files??

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
     // Create a private member variable that can hold our WebView
 
+    int variabel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         /* This code is run when the App is created. Include code that creates your WebView */
