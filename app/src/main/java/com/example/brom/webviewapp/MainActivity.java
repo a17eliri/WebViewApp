c90e4cd1ebcaf89ee0d9279c8045fc6e5d7b39c0
a17eliri <a17eliri@student.his.se>
2018-03-28 10:52:22 +0200

    in MainActivity.java: changed the WebView URL from exernal to internal

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -24,19 +24,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // 1. Create a WebView element in the layout file content_main.xml
-        // 2. Give the WebView element created in step 1 ID "my_webview"
-        // Enter your code that creates your WebView here...
-        // 3. Locate the WebView element created in step 1 using the ID created in step 2
-        // 4. Create a new WebViewClient to attach to our WebView. This allows us to
-        //    browse the web inside our app.
-        // 5. Enter the url to load in our WebView
-
         WebView webView = (WebView) findViewById(R.id.webbfonster);
         WebViewClient blubb = new WebViewClient();
         webView.setWebViewClient(blubb);
 
-        webView.loadUrl("http://m.howrse.com/");
+        // webView.loadUrl("http://m.howrse.com/");
+
+        webView.loadUrl("file:///android_asset/about.html");
     }
 
     @Override
